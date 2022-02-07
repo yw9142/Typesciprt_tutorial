c5017e6936a296bc90be3f53f4a3a37c8cec0af7
Yonghun Park <yw9142@gmail.com>
2022-02-08 02:33:43 +0900

    class

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,35 @@
-const button = document.querySelector('button')!;
+class Department {
+    // private readonly id: string;
+    // private name: string;
+    private employees: string[] = [];
 
-button.addEventListener('click!', () => {
-    console.log('Clicked!');
-});
+    constructor(private readonly id: string, public name: string) {
+        // this.id = id;
+        // this.name = n;
+    }
+
+    describe(this: Department) {
+        console.log(`Department ${this.id}: ${this.name}`);
+    }
+
+    addEmployee(...employees: string[]) {
+        // validation etc
+        this.employees.push(...employees);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+
+const accounting = new Department('d1', 'Accounting');
+
+accounting.addEmployee('Max', 'Manu');
+
+accounting.describe();
+
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// accountingCopy.describe();
