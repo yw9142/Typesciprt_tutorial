8f2df7d3f325ee9d5945b03b31e789ddb1b738bb
Yonghun Park <yw9142@gmail.com>
2023-03-11 14:57:01 +0900

    refactor: 프로젝트 구조 정리

---
+++ src/DnDProject/decorators/autobind.ts
@@ -0,0 +1,13 @@
+namespace App {
+    export function autobind(target: any, methodName: string, descriptor: PropertyDescriptor) {
+        const originalMethod = descriptor.value;
+        const adjDescriptor: PropertyDescriptor = {
+            configurable: true,
+            get() {
+                const boundFn = originalMethod.bind(this);
+                return boundFn;
+            }
+        };
+        return adjDescriptor;
+    }
+}
