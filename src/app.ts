e31684ee38f69c5d3eb684b92e08d79ad86b389c
yw9142@gmail.com <yw9142@gmail.com>
2023-03-18 21:02:41 +0900

    refactor: ES Modules

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+// // Drag & Drop Interface
+
+// Project Type
+
+// Validation
+
+// autobind decorator
+
+// Component Base Class
+
+// ProjectItem Class
+
+// ProjectList Class
+
+// ProejctInput Class
+import { ProjectInput } from "./components/project-input.js";
+import { ProjectList } from "./components/project-list.js";
+
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
---
+++ src/components/project-input.ts
@@ -0,0 +1,64 @@
+import { Component } from "./base-component.js";
+import { validate, Validatable } from "../util/validation.js";
+import { autobind } from "../decorators/autobind.js";
+import { projectState } from "../state/project-state.js";
+
+export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+
+    constructor() {
+        super('project-input', 'app', true, 'user-input');
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+        this.configure();
+    }
+
+    // 보통 public method는 private method 상단에 배치함.
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler);
+    }
+
+    renderContent() { }
+
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        const titleValidatable: Validatable = { value: enteredTitle, required: true };
+        const descriptionValidatable: Validatable = { value: enteredDescription, required: true, minLength: 5 };
+        const peopleValidatable: Validatable = { value: enteredTitle, required: true, min: 1, max: 5 };
+
+        if (
+            !validate(titleValidatable) ||
+            !validate(descriptionValidatable) ||
+            !validate(peopleValidatable)) {
+            alert('Invalid input, please try again!');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
+    @autobind
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, description, people] = userInput;
+            projectState.addProject(title, description, people);
+            // console.log(title, description, people);
+            this.clearInputs();
+        }
+    }
+}
